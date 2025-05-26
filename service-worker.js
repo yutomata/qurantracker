d613ded78774ffb5d64ee26b56551eb66b162363
yutomata <yasir@automata.tech>
2025-05-26 20:59:35 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("sheet-cache-v1").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./styles.css",
+        "./app.js",
+        "./manifest.json",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
